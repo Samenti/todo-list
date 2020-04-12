e10796ed2861317a5298005c91f8870f65362ba2
Raznor <62889081+Raznor@users.noreply.github.com>
2020-04-12 18:35:48 +0200

    added button functionality for each list item

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
 $('button').click(() => {
-    $('ul').append('<li class="created">' + $('#text-input').val() + '<div class="trash-button"></div><div class="done-button"></div></li>');
-    $('#text-input').val('');
-    $('.created').on('click', function(){
-        $(event.target).css('text-decoration', 'line-through');
-    });
+    if ($('#text-input').val() !== '') {
+        $('#temp-item').remove();
+        $('ul').append('<li class="created"><div class="item-text">'  + $('#text-input').val() + '</div><img src="trash_lighter.svg" alt="" class="trash-button"><img src="done_lighter.svg" alt="" class="done-button"></li>');
+        $('#text-input').val('');
+        $('.created').children('.done-button').click(() => {
+            $(event.currentTarget).siblings('.item-text').css('text-decoration', 'line-through');
+            $(event.currentTarget).attr('src', 'done_clicked.svg');
+        });
+
+        $('.created').children('.trash-button').click(() => {
+            $(event.currentTarget).parent().remove();
+        });
+    }
 });
 
-$('li').click(() => {
-    $(event.target).css('text-decoration', 'line-through');
+$('.done-button').click(() => {
+    $(event.currentTarget).siblings('.item-text').css('text-decoration', 'line-through');
+    $(event.currentTarget).attr('src', 'done_clicked.svg');
 });
+
+$('.trash-button').click(() => {
+    $(event.currentTarget).parent().remove();
+});
+
+// $('.done-button').click(doneHandler());
+
+// $('.trash-button').click(trashHandler());
+
+// function doneHandler() {
+//     $(event.currentTarget).prev().css('text-decoration', 'line-through');
+//     $(event.currentTarget).attr('src', 'done_clicked.svg');
+// }
+
+// function trashHandler() {
+//     $(event.currentTarget).parent().remove();
+// }
